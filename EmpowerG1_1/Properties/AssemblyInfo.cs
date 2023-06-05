616c20c75369f2d504f4253fef15e1a5a8dbbbfd
Agus Setiawan <Agus Setiawan@LAPTOP-G9PNSII9>
2023-06-05 13:35:20 +0700

    Add project files.

---
+++ EmpowerG1_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerG1_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerG1_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("438E8F4E-B2B9-4BEE-A57D-47D75EFB78E3")]
+[assembly: AssemblyVersion("1.0.0.0")]
